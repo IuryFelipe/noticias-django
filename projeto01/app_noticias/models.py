3c0f20ba87565866ec2e799232118d7a130a2071
Iury Souza <iurycomputacao@gmail.com>
2022-04-01 10:50:31 -0300

    Create Noticia model

--- projeto01/app_noticias/models.py
+++ projeto01/app_noticias/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Noticia(models.Model):
+    titulo = models.CharField("Título", max_length=128, null = True, blank = True)
+    conteudo = models.TextField("Conteúdo")
