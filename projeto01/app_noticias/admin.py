c206e598cc837aa615374706420763e10627701a
Iury Souza <iurycomputacao@gmail.com>
2022-04-07 19:58:41 -0300

    Registrando a classe Pessoa

--- projeto01/app_noticias/admin.py
+++ projeto01/app_noticias/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Noticia
+from .models import Pessoa
 
 # Register your models here.
 @admin.register(Noticia)
 class NoticiaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Pessoa)
+class PessoaAdmin(admin.ModelAdmin):
     pass
