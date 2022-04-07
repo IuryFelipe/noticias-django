b56596eb92899c1932118cb0357ef8e95a68637e
Iury Souza <iurycomputacao@gmail.com>
2022-04-07 19:48:49 -0300

    Adicionando validadores

--- projeto01/app_noticias/models.py
+++ projeto01/app_noticias/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Noticia(models.Model):
-    titulo = models.CharField("Título", max_length=128)
+    titulo = models.CharField("Título", max_length=128, null = True, blank = True)
     conteudo = models.TextField("Conteúdo")
     data_de_pubicacao = models.DateField("Data de publicação", blank=True, null=True)
