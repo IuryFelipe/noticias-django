55efdf7153cfdce415b370d1dcbaa5e3ac28f792
Iury Souza <iurycomputacao@gmail.com>
2022-04-28 21:32:42 -0300

    Criação de views

--- projeto01/app_noticias/views.py
+++ projeto01/app_noticias/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from app_noticias.models import Pessoa
+
+def resumo_pessoas(request):
+    total = Pessoa.objects.count()
+    return HttpResponse('O total de pessoas é {}' .format(total))
+
+def index(request):
+    return HttpResponse('Página inicial')
